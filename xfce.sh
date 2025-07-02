7e0d9b5c1e66b81c61402ace7c779358957c9426
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2025-07-03 00:15:16 +0800

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -5,7 +5,7 @@ error_exit() { echo "ERROR: $1" >&2; exit 1 }
 if [[ $EUID -ne 0 ]]; then error_exit "This script must be run as root."; fi
 
 pacman -Syu --noconfirm || error_exit "Failed to update system."
-pacman -S --noconfirm "xorg xorg-server xfce4 lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings chromium fastfetch htop" || error_exit "Failed to install XFCE packages."
+pacman -S --noconfirm "xorg xorg-server xfce4 lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings chromium fastfetch" || error_exit "Failed to install XFCE packages."
 pacman -S --noconfirm --needed $(pacman -Sgq xfce4-goodies | grep -v -E "^(parole|xfburn|xfce4-dict)$") || error_exit "Failed to install XFCE goodies"
 systemctl enable lightdm || error_exit "Failed to enable LightDM display manager."
 reboot
