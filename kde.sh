ad6eb40e6d78b27d9c34c9e1e266d4708bc7add8
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2025-07-03 16:40:53 +0800

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -4,4 +4,5 @@ if [[ $EUID -ne 0 ]]; then err "This script must be run as root."; fi
 pacman -Syu --noconfirm || err "Failed to update system."
 pacman -S --noconfirm "xorg xorg-server plasma-desktop plasma-workspace sddm chromium fastfetch htop" || err "Failed to install KDE packages."
 systemctl enable sddm || err "Failed to enable SDDM display manager."
-pacman -Sc; reboot
+pacman -Sc
+reboot
