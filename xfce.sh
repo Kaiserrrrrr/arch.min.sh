afc79d8d4fe42478dfbc06f04228d1c3a571e351
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2025-07-03 21:48:07 +0800

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PKG="xorg xorg-server xfce4 lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings chromium fastfetch"
+PKG="xorg xorg-server xfce4 lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings chromium discord fastfetch"
 err() { echo "ERROR: $1" >&2; exit 1 }
 if [[ $EUID -ne 0 ]]; then err "This script must be run as root."; fi
 pacman -Syu --noconfirm || err "Failed to update system."
