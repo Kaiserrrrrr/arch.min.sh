6596c55cf0fd8f04963282f12ef3cf0f51b18708
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2025-07-03 09:30:48 +0800

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -5,4 +5,4 @@ pacman -Syu --noconfirm || err "Failed to update system."
 pacman -S --noconfirm "xorg xorg-server xfce4 lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings chromium fastfetch" || err "Failed to install XFCE packages."
 pacman -S --noconfirm --needed $(pacman -Sgq xfce4-goodies | grep -v -E "^(parole|xfburn|xfce4-dict)$") || err "Failed to install XFCE goodies"
 systemctl enable lightdm || err "Failed to enable LightDM display manager."
-reboot
+pacman -Sc; reboot
